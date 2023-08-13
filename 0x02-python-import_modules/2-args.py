831595713bbba153550458fc1e98e1724682d526
lazy_coder <ayomidesufian@gmail.com>
2023-08-12 17:59:59 -0700

    python module tasks

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    argv_c = len(argv)
+
+    if argv_c == 1:
+        print("0 arguments.")
+    elif arg_c == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(argv_c - 1))
+
+        for val in range(1, argv_c):
+            print("{}: {}".format(val, argv[val]))
