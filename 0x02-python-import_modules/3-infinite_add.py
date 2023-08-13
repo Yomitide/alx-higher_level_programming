b5142f5c8399fc93f5108a3ca06ab12e6fb60e7f
lazy_coder <ayomidesufian@gmail.com>
2023-08-12 19:09:25 -0700

    python tasks

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,8 +3,8 @@ if __name__ == "__main__":
     from sys import argv
 
     argv_c = len(argv)
-    num_sum = 0
+    numbers_sum = 0
 
     for val in range(1, argv_c):
-        num_sum += int(argv[val])
-        print(num_sum)
+        numbers_sum += int(argv[val])
+        print(numbers_sum)
