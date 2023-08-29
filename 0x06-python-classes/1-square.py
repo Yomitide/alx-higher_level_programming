e57f5b7e935240d8a866404a9e8f5b9eab4333b2
yomitide <ayomidesufian@gmail.com>
2023-08-29 15:45:11 -0700

    square physical attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A class square that defines a square with physical attribute of size
+"""
+class Square:
+    """
+    class with the physical attribute of size
+    """
+    def __init__(self, size=""):
+
+        self.__size = size
