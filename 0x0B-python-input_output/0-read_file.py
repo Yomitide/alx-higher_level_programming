52c515ee868bb736c5084f97ae0e48b4c128932c
yomitide <ayomidesufian@gmail.com>
2023-09-12 09:10:31 -0700

    read and write

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A function that reads a test file utf8 and prints to stdout"""
+
+def read_file(filename=""):
+    """The function using with statement to read a file"""
+    with open(filename, mode="r", encoding="UTF-8") as f:
+        print(f.read(), end="")
