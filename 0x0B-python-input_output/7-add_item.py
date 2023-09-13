931eb163b1b35373a661cab186832f3b1c845a9a
yomitide <ayomidesufian@gmail.com>
2023-09-13 14:57:58 -0700

    json

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" A function that adds all arguments to a Python
+    list, and then save them to a file:
+"""
+import sys
+import os
+save_to_json = __import__("5-save_to_json_file").save_to_json_file
+load_from_json = __import__("6-load_from_json_file").load_from_json_file
+
+
+def add_item(args, filename):
+    if (os.path.exists(filename)):
+        existing_content = load_from_json(filename)
+    else:
+       existing_content = []
+    existing_content.extend(args)
+    save_to_json(existing_content + argv[1:], filename)
