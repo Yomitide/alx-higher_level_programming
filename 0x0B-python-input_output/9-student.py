0fc144e20fc734f5979ec6944d275f2a3255ac5b
yomitide <ayomidesufian@gmail.com>
2023-09-13 15:25:42 -0700

    json classes

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" A module that defines a class student by public instances"""
+
+class Student():
+    """class created with public instances first_name,last_name,age"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialising the public instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """that retrieves a dictionary representation of a Student instance"""
+        return self.__dict__
